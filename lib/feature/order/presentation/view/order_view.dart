51c0004d01f65f4fa4bd7f084077e66aee84e467
unknown <ayanasser1853@gmail.com>
2025-08-17 10:20:15 +0300

    add order view ui

--- lib/feature/order/presentation/view/order_view.dart
+++ lib/feature/order/presentation/view/order_view.dart
@@ -15,64 +15,76 @@ class OrderView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: abbBar(title: 'My Order'),
-      body: Column(
-        children: [
-          CustomContainer(
-            child: Row(
-              spacing: 20.h,
-              children: [
-                CustomCircleContainer(
-                  height: 130,
-                  color: AppColors.deliveringStatusBackGroundColor,
-                  widget: Image.asset(AppAssets.deliveringStatusIcon),
-                ),
-                Column(
-                  spacing: 7.h,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('#8882610', style: AppTextStyle.titilliumWebBold),
-
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 10.w,
-                      children: [
-                        Text("9 Sep", style: AppTextStyle.titilliumWebRegular),
-                        CircleAvatar(
-                          backgroundColor: AppColors.greyShaded500,
-                          radius: 2,
-                        ),
-                        Text(
-                          '4 items',
-                          style: AppTextStyle.titilliumWebRegular,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 10.w,
-                      children: [
-                        Text(
-                          "Status:",
-                          style: AppTextStyle.titilliumWebRegular,
-                        ),
-
-                        Text(
-                          'Delivering',
-                          style: AppTextStyle.titilliumWebRegular.copyWith(
-                            color: AppColors.deliveringStatusdColor,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            CustomContainer(
+              child: Row(
+                spacing: 10.h,
+                children: [
+                  CustomCircleContainer(
+                    height: 80,
+                    color: AppColors.deliveringStatusBackGroundColor,
+                    widget: Image.asset(AppAssets.deliveringStatusIcon),
+                  ),
+                  Column(
+                    spacing: 7.h,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('#8882610', style: AppTextStyle.titilliumWebBold),
+        
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 5.w,
+                        children: [
+                          Text("9 Sep", style: AppTextStyle.titilliumWebRegular),
+                          CircleAvatar(
+                            backgroundColor: AppColors.greyShaded500,
+                            radius: 2,
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Image.asset(AppAssets.deliveringStatusIcon),
-              ],
+                          Text(
+                            '4 items',
+                            style: AppTextStyle.titilliumWebRegular,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 3.w,
+                        children: [
+                          Text(
+                            "Status:",
+                            style: AppTextStyle.titilliumWebRegular,
+                          ),
+        
+                          Text(
+                            'Delivering',
+                            style: AppTextStyle.titilliumWebRegular.copyWith(
+                              color: AppColors.deliveringStatusdColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  CustomCircleContainer(
+                    color: AppColors.deliveringStatusdColor,
+                    height: 80,
+                    widget: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: AppColors.white,width: 2,),
+                        borderRadius: BorderRadius.circular(8),
+                        color: AppColors.deliveringStatusdColor
+                      ),
+                      child: Icon(Icons.arrow_forward_ios_rounded,color: AppColors.white,))),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
